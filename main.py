273450dcc050b5ae08e7876a200fa96ea1bff3c7
Artur Z <arturziianbaev@gmail.com>
2023-10-14 18:45:55 -0700

    print top 10 colors as RGB and HEX values from SELECTED image CLEAN VERSION

--- main.py
+++ main.py
@@ -21,15 +21,14 @@ def select():
     filename=fd.askopenfilename(title='Select IMG', initialdir='/', filetypes=filetypes)
     # print(filename)
     place_image(filename)
+    #read colors from that image
     dominants.image_path=filename
     dominants.dominant_colors(10)
-    # bg = "rgb(0, 255, 0)",
+    #print out result
     x=1
     for color in dominants.colors:
         print(f"Top {x} color as RGB: {color}\nas #HEX: {_from_rgb(color)}")
         x+=1
-    # print(dominants.colors)
-
 
 def place_image(path):
     global picture,start_picture_img
@@ -37,10 +36,6 @@ def place_image(path):
     picture = picture.resize((300, 300))
     picture = ImageTk.PhotoImage(picture)
     start_picture_img.config(image=picture)
-
-# def dominant_colors(number_of_colors):
-#
-
 
 window=tk.Tk()
 window.title('Image to Color Palette')
@@ -54,19 +49,11 @@ label.grid(row=0,column=0,pady=20,padx=100)
 select_button=tk.Button(window, text='Select Image',command=select)
 select_button.grid(row=1,column=0,pady=20)
 
-# start_picture=tk.PhotoImage(file='start_picture.jpg')
-# start_picture.
-
 start_picture=Image.open('start_picture.jpg')
 start_picture=start_picture.resize((300,300))
 start_picture=ImageTk.PhotoImage(start_picture)
 
-
 start_picture_img=tk.Label(image=start_picture)
 start_picture_img.grid(row=2,column=0)
-
-
-# top_1=tk.Label(text='Top 1',fg="white")
-# top_1.grid(row=4,column=0)
 
 window.mainloop()
