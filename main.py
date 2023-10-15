10648becc18396ee596aac80667ad836842c2a54
Artur Z <arturziianbaev@gmail.com>
2023-10-14 20:08:54 -0700

    top 10 colors GUI with HEX code as label

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def _from_rgb(rgb):
     return "#%02x%02x%02x" % rgb
 
 def select():
-    global filename
+    global filename, colors
     filetypes = (
         ("Image files", "*.jpg *.jpeg *.png"),
         ("All files", "*.*")
@@ -25,10 +25,11 @@ def select():
     dominants.image_path=filename
     dominants.dominant_colors(10)
     #print out result
-    x=1
-    for color in dominants.colors:
-        print(f"Top {x} color as RGB: {color}\nas #HEX: {_from_rgb(color)}")
-        x+=1
+
+    for index, item in enumerate(dominants.colors):
+
+        print(f"Top {index+1} color as RGB: {item}\nas #HEX: {_from_rgb(item)}")
+        colors[index].config(text=_from_rgb(item),bg=_from_rgb(item), fg='black')
 
 def place_image(path):
     global picture,start_picture_img
@@ -39,9 +40,9 @@ def place_image(path):
 
 window=tk.Tk()
 window.title('Image to Color Palette')
-window.geometry("500x500")
-window.resizable(False, False)
-
+window.geometry("500x600")
+window.resizable(True, True)
+window.config(bg='#F3F0CA')
 label=tk.Label(text='THIS SIMPLE APP HELPS YOU TO EXTRACT \n TOP 10 COLORS '
                     'FROM ANY PICTURE',font=('Courier',10,'bold'))
 label.grid(row=0,column=0,pady=20,padx=100)
@@ -55,5 +56,14 @@ start_picture=ImageTk.PhotoImage(start_picture)
 
 start_picture_img=tk.Label(image=start_picture)
 start_picture_img.grid(row=2,column=0)
+
+frame=tk.Frame(window,width=300,height=50,bg='grey')
+frame.grid(row=3,column=0,pady=20)
+
+colors=[]
+for _ in range(10):
+    label_=tk.Label(frame,text="Color",bg='#F3F0CA',fg='#F3F0CA')
+    label_.grid(row=0,column=_, padx=0)
+    colors.append(label_)
 
 window.mainloop()
